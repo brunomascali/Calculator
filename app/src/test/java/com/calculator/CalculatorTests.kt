cf4ceb4a6e5682c6dea7601cafb180b11329e5dc
Bruno Mascali <brunomascalii@gmail.com>
2025-08-22 19:01:36 -0300

    Parenthesis

--- app/src/test/java/com/calculator/CalculatorTests.kt
+++ app/src/test/java/com/calculator/CalculatorTests.kt
@@ -7,27 +7,27 @@ class CalculatorTests {
     @Test
     fun simple_add() {
         val input = "1+2"
-        assertEquals(Calculator.eval(parse(input)), 3)
+        assertEquals(Calculator.eval(parse(input)), 3.0, 1e-10)
     }
 
     @Test
     fun simple_sub() {
         val input = "1-2"
-        assertEquals(Calculator.eval(parse(input)), -1)
+        assertEquals(Calculator.eval(parse(input)), -1.0, 1e-10)
     }
 
     @Test
     fun simple_mul() {
         val input = "2*3"
-        assertEquals(Calculator.eval(parse(input)), 6)
+        assertEquals(Calculator.eval(parse(input)), 6.0, 1e-10)
     }
 
     @Test
     fun precedence() {
         var input = "1+2*3"
-        assertEquals(Calculator.eval(parse(input)), 7)
+        assertEquals(Calculator.eval(parse(input)), 7.0, 1e-10)
         input = "1*2+3"
-        assertEquals(Calculator.eval(parse(input)), 5)
+        assertEquals(Calculator.eval(parse(input)), 5.0, 1e-10)
     }
 
     @Test
@@ -40,5 +40,11 @@ class CalculatorTests {
     fun sqrt() {
         val input = "√4"
         assertEquals(Calculator.eval(parse(input)), 2.0, 1e-10)
+    }
+
+    @Test
+    fun parenthesis() {
+        val input = "2*(3+7)"
+        assertEquals(Calculator.eval(parse(input)), 20.0, 1e-10)
     }
 }
